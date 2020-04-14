6b95e784b94c6b27660daf2a0bc5aaa6860a8cd5
takayak <tktknet0530@gmail.com>
2020-04-14 17:42:27 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -33,7 +33,7 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
     config.fog_directory  = 'lunch500s3' #S3のバケット名
-    config.asset_host = 'https://s3-your_region.amazonaws.com/lunch500s3'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/lunch500s3'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
