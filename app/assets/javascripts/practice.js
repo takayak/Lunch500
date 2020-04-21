d3329dba5722403aca1b281420d6fb6dc74dbbad
takayak <tktknet0530@gmail.com>
2020-04-21 20:36:37 +0900

    add
    add

--- app/assets/javascripts/practice.js
+++ app/assets/javascripts/practice.js
@@ -1,33 +1,33 @@
-{
-  function update() {
+// {
+//   function update() {
 
-    document.querySelector('h1').textContent = 'changed!';
-    querySelector = 最初のものだけ
-    querySelectorALL('p')[1].textContent = 'hello';
-    querySelectorALL('p').forEach((p,index) => {
-      p.textContent = '${index}番目のpです';
-    });
-    // document.querySelector('button').addEventListener('click',update);
-    document.querySelector('button').addEventListener('click',()=>{
-      document.getElementById('target').textContent = 'changed';
-    });
+//     document.querySelector('h1').textContent = 'changed!';
+//     querySelector = 最初のものだけ
+//     querySelectorALL('p')[1].textContent = 'hello';
+//     querySelectorALL('p').forEach((p,index) => {
+//       p.textContent = '${index}番目のpです';
+//     });
+//     // document.querySelector('button').addEventListener('click',update);
+//     document.querySelector('button').addEventListener('click',()=>{
+//       document.getElementById('target').textContent = 'changed';
+//     });
     
 
-    document.getElementById('target').textContent = 'changed!';
-    .className = 'my-color';
-    .classList.add('mycolor');
-    .classList.contain('my-color') ===true){
-      .classList.add.remove
+//     document.getElementById('target').textContent = 'changed!';
+//     .className = 'my-color';
+//     .classList.add('mycolor');
+//     .classList.contain('my-color') ===true){
+//       .classList.add.remove
 
-      targetNode.classList.toggle('mycolor');
-      targetNode.textContent = 'dotinstall';
-      カスタムデータ属性
-      data-translation = 'dotinstall';
-    }
-  }
+//       targetNode.classList.toggle('mycolor');
+//       targetNode.textContent = 'dotinstall';
+//       カスタムデータ属性
+//       data-translation = 'dotinstall'
+//     }
+//   }
 
-  setTimeout(update.1000);
+//   setTimeout(update.1000);
 
-}
+// }
 
 
