097984f74a2efbcba4025dfb52c42fab332dab15
takayak <tktknet0530@gmail.com>
2020-04-30 13:36:32 +0900

    ゲストログイン機能
    ゲストログイン機能の追加

---
+++ app/controllers/guest_controller.rb
@@ -0,0 +1,26 @@
+class GuestController < ApplicationController
+
+  def create
+    @user = User.find_by(email: 'test@test')
+    if @user == nil
+      flash[:notice] = 'ゲスト購入ユーザーが作成されていません。'
+      redirect_to posts_path
+    else
+      sign_in(:user, @user)
+      flash[:notice] = 'ゲスト購入ユーザーでログインしました'
+      redirect_to posts_path
+    end
+  end
+
+  def create2
+    @user = User.find_by(email: 'test@test2')
+    if @user == nil
+      flash[:notice] = 'ゲスト購入ユーザーが作成されていません。'
+      redirect_to posts_path
+    else
+      sign_in(:user, @user)
+      flash[:notice] = 'ゲスト購入ユーザーでログインしました'
+      redirect_to posts_path
+    end
+  end
+end
