e316e8d4715877af2e38f160ba2d98cc5ec6ef72
takayak <tktknet0530@gmail.com>
2020-05-01 16:40:23 +0900

    likeの非同期
    likeの非同期

---
+++ app/assets/javascripts/like.js
@@ -0,0 +1,9 @@
+$(document).on('turbolinks:load', function () {
+  $(function(){
+    $(.fileField).on('change', $fileField, function(e) {
+      file = e.target.files[0]
+      reader = new FileReader(),
+      $preview = $("#img_field");
+  });
+  });
+});
