650df0b3ce8fda91fc3d2b3d7f2faa07f0b05640
takayak <tktknet0530@gmail.com>
2020-05-24 23:04:41 +0900

    js記述コメントアウト
    js記述コメントアウト
    ノードの名前が違うかurlが違うため動いていないです。

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,26 +1,26 @@
-$(function() {
-  $('.top').on('click', function(e) {
-    // e.preventDefault();
-    var comment = $('#comment_text').val();
-    console.log(comment);
+// $(function() {
+//   $('.top').on('click', function(e) {
+//     // e.preventDefault();
+//     var comment = $('#comment_text').val();
+//     console.log(comment);
 
-    $.ajax({
-      type: 'POST',
-      url: '/comments/',
-      data: {
-        comment: {
-          content: comment
-        }
-      },
-      dataType: 'json'
-    })
-    .done(function(data) {
-      var html = $('<div class="comment">').append(data.content);
-      $('.flex-box').append(html);
-      $('.comment_text').val('');
-    })
-    .fail(function() {
-      alert('error');
-    });
-  });
-});
+//     $.ajax({
+//       type: 'POST',
+//       url: '/comments/',
+//       data: {
+//         comment: {
+//           content: comment
+//         }
+//       },
+//       dataType: 'json'
+//     })
+//     .done(function(data) {
+//       var html = $('<div class="comment">').append(data.content);
+//       $('.flex-box').append(html);
+//       $('.comment_text').val('');
+//     })
+//     .fail(function() {
+//       alert('error');
+//     });
+//   });
+// });
