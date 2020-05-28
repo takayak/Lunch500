b5568045466161b03a0c58abfbdce90cb910ec5c
takayak <tktknet0530@gmail.com>
2020-05-28 22:36:25 +0900

    インクリメンタルサーチ
    インクリメンタルサーチ

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,25 @@
 $(function() {
+  var search_list = $(".main");
+
+  function appendpost(post) {
+    var html = `
+    <% posts.each do |post|%>
+                <div class="post hover hover4">
+                  <img src="${post}">
+                </div>
+                <% end%>
+                  `
+    search_list.append(html);
+  }
+
+
+
+
+  function appendErrMsgToHTML(msg) {
+    var html = `<div class='name'>${ msg }</div>`
+    search_list.append(html);
+  }
+
   $(".search-input").on("keyup", function() {
     var input = $(".search-input").val();
     console.log(input);
@@ -10,6 +31,16 @@ $(function() {
     })
     .done(function(posts) {
       console.log(posts);
+    // })
+      $(".main").empty();
+      if (posts.length !== 0) {
+        posts.forEach(function(post){
+          appendpost(post);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するツイートがありません");
+      }
     })
     .fail(function() {
       console.log('ユーザー検索に失敗しました');
--- app/views/posts/search.json.jbuilder
+++ app/views/posts/search.json.jbuilder
@@ -3,6 +3,13 @@ json.array! @posts do |post|
   json.content post.content
   json.image post.image
   json.user_id post.user_id
+  json.address post.address
+  json.latitude post.latitude
+  json.longitude post.longitude
+  json.title post.title
+  json.category post.category_id
+  json.created_at post.created_at
+  json.updated_at post.updated_at
   # json.nickname post.user.nickname
   # json.user_sign_in current_user
 end
