554099961e434cac43fa6d1b4878d1e61c08484c
takayak <tktknet0530@gmail.com>
2020-06-01 00:31:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'maps/index'
   # get 'chart/index'
-  devise_for :users
+  # devise_for :users
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
