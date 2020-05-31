f379e62e6136d59ed4c99f5ae74720a9dee6ac3d
takayak <tktknet0530@gmail.com>
2020-05-31 21:12:30 +0900

    遷移先エラー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    posts_path
+  end 
+  #ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource)
+    posts_path 
+  end 
   
   protected
   def configure_permitted_parameters
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,13 +3,7 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  def after_sign_in_path_for(resource)
-    posts_path 
-  end 
-  #ログアウト後のリダイレクト先
-  def after_sign_out_path_for(resource)
-    posts_path 
-  end 
+  
 
   # GET /resource/sign_in
   # def new
