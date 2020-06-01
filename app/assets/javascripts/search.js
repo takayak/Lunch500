be41a79a74370b1e4210a743bdad1ec25c8b1bd3
takayak <tktknet0530@gmail.com>
2020-06-01 20:29:19 +0900

    Update search.js

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,50 +1,50 @@
-$(function() {
-  var search_list = $(".main");
+// $(function() {
+//   var search_list = $(".main");
 
-  function appendpost(post) {
-    var html = `
-    <% posts.each do |post|%>
-                <div class="post hover hover4">
-                  <img src="${post}">
-                </div>
-                <% end%>
-                  `
-    search_list.append(html);
-  }
-
-
+//   function appendpost(post) {
+//     var html = `
+//     <% posts.each do |post|%>
+//                 <div class="post hover hover4">
+//                   <img src="${post}">
+//                 </div>
+//                 <% end%>
+//                   `
+//     search_list.append(html);
+//   }
 
 
-  function appendErrMsgToHTML(msg) {
-    var html = `<div class='name'>${ msg }</div>`
-    search_list.append(html);
-  }
 
-  $(".search-input").on("keyup", function() {
-    var input = $(".search-input").val();
-    console.log(input);
-    $.ajax({
-      type: 'GET',
-      url: '/posts/search',
-      data: { keyword: input },
-      dataType: 'json'
-    })
-    .done(function(posts) {
-      console.log(posts);
-    // })
-      $(".main").empty();
-      if (posts.length !== 0) {
-        posts.forEach(function(post){
-          appendpost(post);
-        });
-      }
-      else {
-        appendErrMsgToHTML("一致するツイートがありません");
-      }
-    })
-    .fail(function() {
-      console.log('ユーザー検索に失敗しました');
-      // alert('ユーザー検索に失敗しました');
-    });
-  });
-});
+
+//   function appendErrMsgToHTML(msg) {
+//     var html = `<div class='name'>${ msg }</div>`
+//     search_list.append(html);
+//   }
+
+//   $(".search-input").on("keyup", function() {
+//     var input = $(".search-input").val();
+//     console.log(input);
+//     $.ajax({
+//       type: 'GET',
+//       url: '/posts/search',
+//       data: { keyword: input },
+//       dataType: 'json'
+//     })
+//     .done(function(posts) {
+//       console.log(posts);
+//     // })
+//       $(".main").empty();
+//       if (posts.length !== 0) {
+//         posts.forEach(function(post){
+//           appendpost(post);
+//         });
+//       }
+//       else {
+//         appendErrMsgToHTML("一致するツイートがありません");
+//       }
+//     })
+//     .fail(function() {
+//       console.log('ユーザー検索に失敗しました');
+//       // alert('ユーザー検索に失敗しました');
+//     });
+//   });
+// });
